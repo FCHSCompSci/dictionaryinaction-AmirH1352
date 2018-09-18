fd118da1fa5d430bb83930aa74e3edb418e8e06e
Amir Hussein <amir.hussein@stu.jefferson.kyschools.us>
2018-09-18 08:00:40 -0400

    dictionary commit fc

---
+++ Dictionaries in Action.py
@@ -0,0 +1,30 @@
+list_of_locations = {
+    'louiville':ky,
+    'california':sanfran,
+    'new york': timesquare
+    }
+colors = {
+    c1: 'blue',
+    c2: 'black',
+    c3: 'white',
+    c4: 'red'
+    }
+
+version = {
+    v1: 'manison',
+    v2: 'apartment',
+    v3: 'hotel'
+    }
+
+
+print =( "welcome to make a new home")
+while True:
+    print = input("please type your name or press x to exit")
+    if input == "x":
+        break
+    else:
+        print (" Hi %s you will be given a random location"%(name))
+    
+    
+    
+
