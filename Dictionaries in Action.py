2e51bf708075c8309c70944b92cb7500d1b1f286
Amir Hussein <amir.hussein@stu.jefferson.kyschools.us>
2018-09-20 07:33:59 -0400

    dictionary fc

--- Dictionaries in Action.py
+++ Dictionaries in Action.py
@@ -1,40 +1,54 @@
 import random
 
+final_home = [ ]
 locations = {
-    'louiville':ky,
-    'california':sanfran,
-    'new york': timesquare
+    'louiville':'ky',
+    'california':'sanfran',
+    'new york': 'timesquare'
     }
 colors = {
-    c1: 'blue',
-    c2: 'black',
-    c3: 'white',
-    c4: 'red'
+    'c1': 'blue',
+    'c2': 'black',
+    'c3': 'white',
+    'c4': 'red'
     }
 
 version = {
-    v1: 'manison',
-    v2: 'apartment',
-    v3: 'hotel'
+    'v1': 'manison',
+    'v2': 'apartment',
+    'v3': 'hotel'
     }
 
 print ( "welcome to make a new home")
 while True:
     user_name = input("please type your name or press x to exit")
     if user_name == "x":
-        break
+            break
     else:
-        print (" Hi %s you will be given a random location"%(user_name))
-
-print ("%s is your homes location"%(random.choice(locations.keys())
+            print (" Hi %s you will be given a random location"%(user_name))
+print ("%s is the location for your house"%(random.choice(locations.keys))
+               
 def build_house(color,style):
-        color = input ("here are a list of colors, pick one %s"%(colors))
+    build_house.split(",")
+    for key,value in colors.items():
+        color = input ("Here are a list of colors, pick one(type c than number to pick one) %s %s"%(key,value))
             if color == 'black':
                 return "c2"
             elif color == 'blue':
                 return "c1"
             elif color == 'white':
-                return "c3"
+                return ""
             elif color == 'red':
-                return "c4"
+                return "red paint"
+    for key,value in version.item():
+        style = input ("also choose a style for you house (t %s"%(version))
+            if style == 'mansion':
+                return "mansion home"
+            elif style == 'apartment':
+                return "apartment home"
+            elif style == 'hotel':
+                return "hotel home"
+final_home.append( "%s,%s"%(color,style))
+                                            
+print ("Here is you new home %s"%(final_home))   
                                     
